daeaee91835cae481e7b53cc2717d4a9a7843cf9
mishra <tech@golivelabs.io>
2015-05-12 12:39:56 -0700

    uploads

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,16 @@
 var router = require('express').Router();
+var multer = require('multer');
+router.use(multer({
+    dest: './uploads/'
+}));
 
 router.get('/', function (req, res) {
+    res.render('upload');
+});
+
+router.post('/', function (req, res) {
+    console.log(req.files);
+    console.log();
     res.render('upload');
 });
 
